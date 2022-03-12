bb924f735134ef5d07cc4e5a2877bd79e8c41db0
Josilda Gomes <88357822+JosildaGomes@users.noreply.github.com>
2022-03-12 12:30:15 -0300

    Atualização

---
+++ u1_exercicio3/cadpessoas_josilda/src/main/java/br/com/josilda/cadpessoas/CadpessoasJosildaApplication.java
@@ -0,0 +1,13 @@
+package br.com.josilda.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasJosildaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasJosildaApplication.class, args);
+	}
+
+}
