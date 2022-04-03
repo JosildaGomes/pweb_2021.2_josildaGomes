459cd0b63079686aa9e521004ce50c1da5b23652
PWeb UFERSA <pwebufersa@gmail.com>
2022-04-02 23:36:57 -0300

    Arrumando exercicio 3

--- u1_exercicio3/cadpessoas_josilda/src/main/java/br/com/josilda/cadpessoas/CadpessoasJosildaApplication.java
+++ u1_exercicio3/cadpessoas_josilda/src/main/java/br/com/josilda/cadpessoas/CadpessoasJosildaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CadpessoasJosildaApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(CadpessoasJosildaApplication.class, args);
 	}
--- u1_exercicio3/cadpessoas_josilda/src/main/java/br/com/josilda/cadpessoas/repositories/PessoaRepository.java
+++ u1_exercicio3/cadpessoas_josilda/src/main/java/br/com/josilda/cadpessoas/repositories/PessoaRepository.java
@@ -1,12 +1,11 @@
-package br.com.josilda.cadpessoas.repositories; 
+package br.com.josilda.cadpessoas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.josilda.cadpessoas.model.Pessoa; 
- 
-@Repository 
+import br.com.josilda.cadpessoas.model.Pessoa;
 
-public interface PessoaRepository extends JpaRepository <Pessoa, Long>{ 
-     
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
 }
